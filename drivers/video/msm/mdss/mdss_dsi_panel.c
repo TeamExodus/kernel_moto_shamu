929b68a305efb7ae090e9165615e046348f4fefa
wSedlacek <wsedlacek1@gmail.com>
2015-08-01 11:20:09 -0700

    [KERNEL] Adreno Idler
    msm_adreno_tz: be aware of suspended state
    
    Force to use & record as min freq when system has
    entered pm-suspend or screen-off state.
    
    Introduce Adreno idler for devfreq-based Adreno devices
    
    Adreno idler by arter97 - Idling algorithm,
    an efficient workaround for msm-adreno-tz's overheads.
    
    Main goal is to lower the power consumptions while maintaining high-performance.
    
    Since msm-adreno-tz tends to *not* use the lowest frequency even on idle,
    Adreno idler replaces msm-adreno-tz's algorithm when it comes to
    calculating idle frequency(mostly by ondemand's method).
    The higher frequencies are not touched with this algorithm, so high-demanding
    games will (most likely) not suffer from worsened performance.
    
    The additional idle_lasttime detects if last 500ms was idle before
    ramping down the frequency to prevent micro-lags on scrolling or playing games.
    
    adreno_idler: switch to count based instead of time based
    
    Asking for current time on every poll(which is usually 10ms)
    is very inefficient.
    
    Switch to count based.
    
    The new default for idlewait is set to 20,
    so every 20 events before must be all idle before Adreno idler
    ramps down the frequency.
    
    adreno_idler: remove frequency bump
    
    Allow msm-adreno-tz to figure out the appropriate frequency for
    the current workload.
    
    We don't need to bump up the frequency within the Adreno idler's
    routine.
    
    This should give some extra plus to power savings.
    
    Bump up to version 1.1.
    
    adreno_idler: fix-up type definitions
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    
    msm_adreno_tz: fix-up tz_suspend
    
    Commit 22d0b352f54a00029cfa18c368e9d680e3aaf60a has introduced many weirdness
    and inconsistencies regardless of Adreno idler being enabled or not.
    
    Partially revert and fix those inconsistencies.
    
    adreno_idler: fix-up some comments
    
    adreno_idler: fix typos :)
    
    adreno idler: use my screen off detection global variable instead
    
    mdss: add variable for screen on/off
    
    Change-Id: Ied33d329dd07ba7b044172023ed4e2b3e2c539db

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -40,6 +40,8 @@
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
+bool mdss_screen_on = true;
+
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	ctrl->pwm_bl = pwm_request(ctrl->pwm_lpg_chan, "lcd-bklt");
@@ -676,6 +678,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
+	mdss_screen_on = true;
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -755,6 +758,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
+	mdss_screen_on = false;
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
