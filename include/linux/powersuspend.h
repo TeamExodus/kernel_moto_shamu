8bf59969b31f0392ab615c4f5ad14de64f92c818
wSedlacek <wsedlacek1@gmail.com>
2015-08-01 00:24:25 -0700

    Meh
    Change-Id: If95f84feba2b8182aa41342dd912d99bb185f0c1

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -24,8 +24,15 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
+<<<<<<< HEAD
 #define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
 #define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
+=======
+#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
+#define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
+#define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
+#define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep as hook
+>>>>>>> d3ebefc... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.5 (faux123/Yank555.lu)
 
 struct power_suspend {
 	struct list_head link;
@@ -36,6 +43,10 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
+<<<<<<< HEAD
+=======
+void set_power_suspend_state_autosleep_hook(int new_state);
+>>>>>>> d3ebefc... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.5 (faux123/Yank555.lu)
 void set_power_suspend_state_panel_hook(int new_state);
 
 #endif
